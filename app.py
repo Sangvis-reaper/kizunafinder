ccab3ec73ba13ca09468f70e9106555e4bd24498
Sangvis-reaper <190318075+Sangvis-reaper@users.noreply.github.com>
2025-03-24 09:02:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,220 +1,73 @@
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT      
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd holeheweb 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ nano email_checker.py 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ ls
-app.py  email_checker.py  env  __pycache__
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ cd ..       
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  filtered_outputs  main.py  OSINT  raw_outputs  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp $ rm -rf filtered_outputs raw_outputs
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT/holeheweb 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ nano email_checker.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ ls
-app.py  email_checker.py  env  __pycache__
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ cd ..             
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt  streamlit-multiapp
-elk@osintelk:/home/elk/streamlit-multiapp $ rm -rf streamlit-multiapp          
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT/holeheweb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ nano email_checker.py 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ ls
-app.py  email_checker.py  env  __pycache__
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ cd ..             
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  mohdfaizal11368@gmail.com_filtered_result.txt  mohdfaizal11368@gmail.com_holehe_result.txt  OSINT  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp $ rm mohdfaizal11368@gmail.com_filtered_result.txt mohdfaizal11368@gmail.com_holehe_result.txt 
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT/holeheweb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ ls
-app.py  email_checker.py  env  __pycache__
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ nano email_checker.py 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ ls
-app.py  email_checker.py  env  __pycache__
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ cd ..             
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd.. 
-zsh: command not found: cd..
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt  results
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT/holeheweb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ nano email_checker.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ ls
-app.py  email_checker.py  env  __pycache__
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/holeheweb $ cd ..             
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt  results
-elk@osintelk:/home/elk/streamlit-multiapp $ rm results                                                                                  
-rm: cannot remove 'results': Is a directory
-elk@osintelk:/home/elk/streamlit-multiapp $ rm -rf results           
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt  results
-elk@osintelk:/home/elk/streamlit-multiapp $ rm -rf results
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT/ghunt 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/ghunt git:(main*) $ ls
-app.py  assets  ghunt  LICENSE.md  main.py  poetry.lock  __pycache__  pyproject.toml  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/ghunt git:(main*) $ nano app.py          
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/ghunt git:(main*) $ nano app.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/ghunt git:(main*) $ cd ..         
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ ls   
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd usernametoolweb 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/usernametoolweb $ ls
-app.py  __pycache__  stream.py  usernametool.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/usernametoolweb $ cd ..             
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt  results
-elk@osintelk:/home/elk/streamlit-multiapp $ cd components 
-elk@osintelk:/home/elk/streamlit-multiapp/components $ ls
-homepage.py  __init__.py  login.py  __pycache__
-elk@osintelk:/home/elk/streamlit-multiapp/components $ nano .env  
-elk@osintelk:/home/elk/streamlit-multiapp/components $ nano homepage.py                                                                                    
-elk@osintelk:/home/elk/streamlit-multiapp/components $ cd ..        
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT/usernametoolweb 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/usernametoolweb $ nano stream.py           
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/usernametoolweb $ nano stream.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/usernametoolweb $ cd ,,                   
-cd: no such file or directory: ,,
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/usernametoolweb $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd sherlock   
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/sherlock git:(master*) $ ls
-app.py  azminali.txt  devel  Dockerfile  docs  LICENSE  __pycache__  pyproject.toml  pytest.ini  sherlock_project  tests  tox.ini  username.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/sherlock git:(master*) $ cd ..      
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-components  env  main.py  OSINT  requirements.txt  results
-elk@osintelk:/home/elk/streamlit-multiapp $ cd components 
-elk@osintelk:/home/elk/streamlit-multiapp/components $ ls
-homepage.py  __init__.py  login.py  __pycache__
-elk@osintelk:/home/elk/streamlit-multiapp/components $ nano homepage.py 
-elk@osintelk:/home/elk/streamlit-multiapp/components $ cd ..        
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT/sherlock       
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/sherlock git:(master*) $ ls              
-app.py  azminali.txt  devel  Dockerfile  docs  LICENSE  __pycache__  pyproject.toml  pytest.ini  sherlock_project  tests  tox.ini  username.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/sherlock git:(master*) $ nano username.py 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/sherlock git:(master*) $ nano username.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/sherlock git:(master*) $ cd ..            
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd Hawkerweb 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/Hawkerweb $ ls
-app.py  env  main.py  __pycache__  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/Hawkerweb $ nano app.py     
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/Hawkerweb $ nano app.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/Hawkerweb $ cd ..       
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd gvision 
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/gvision git:(main*) $ ls
-gvision.png  gvision.py  LICENSE  __pycache__  README.md  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/gvision git:(main*) $ nano gvision.p      
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/gvision git:(main*) $ nano gvision.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/gvision git:(main*) $ nano gvision.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/gvision git:(main*) $ nano gvision.py
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT/gvision git:(main*) $ cd ..     
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd.. 
-zsh: command not found: cd..
-elk@osintelk:/home/elk/streamlit-multiapp/OSINT $ cd ..
-elk@osintelk:/home/elk/streamlit-multiapp $ scp -r /home/elk/streamlit-multiapp user1@10.168.222.29:/home/user1/
+import streamlit as st
+from streamlit_option_menu import option_menu
+import requests
+from urllib.parse import urlparse
 
-ssh: connect to host 10.168.222.29 port 22: Connection timed out
-scp: Connection closed
-elk@osintelk:/home/elk/streamlit-multiapp $ cd OSINT         
-cd: no such file or directory: OSINT
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-streamlit-multiapp
-elk@osintelk:/home/elk/streamlit-multiapp $ cd streamlit-multiapp 
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp git:(main) $ cd streamlit-multiapp
-cd: no such file or directory: streamlit-multiapp
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp git:(main) $ ls
-components  env  main.py  OSINT  README.md  requirements.txt  results
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp git:(main) $ cd OSINT             
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT git:(main) $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT git:(main) $ cd kizunafinder 
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT/kizunafinder git:(main*) $ ls
-app.py  __pycache__  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT/kizunafinder git:(main*) $ nano app.py    
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT/kizunafinder git:(main*) $ cd ..          
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT git:(main) $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT git:(main) $ cd usernametoolweb 
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT/usernametoolweb git:(main) $ ls
-app.py  __pycache__  stream.py  usernametool.py
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT/usernametoolweb git:(main) $ nano stream.py       
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT/usernametoolweb git:(main) $ Connection to mybalishag.zapto.org closed by remote host.
-Connection to mybalishag.zapto.org closed.
-client_loop: send disconnect: Broken pipe
-(base) tohka@Tohkas-MacBook-Pro .ssh % ssh raven@mybalishag.zapto.org -p 22121 -L 5601:192.168.100.147:5601 
-Last login: Fri Mar 21 08:21:35 2025 from 60.53.125.6
-OpenBSD 7.1 (GENERIC.MP) #465: Mon Apr 11 18:03:57 MDT 2022
+# Google Custom Search API Configuration
+API_KEY = "AIzaSyDiA0IAG6bEejxAt4ca0or2cbJfWZIVQB0"  # Replace with your API key
+CSE_ID = "81126ca8728f54cc7"  # Replace with your CSE ID
 
-Welcome to OpenBSD: The proactively secure Unix-like operating system.
+def main():
+    with st.sidebar:
+        selected_option = option_menu(
+            menu_title="Social Media Platforms",
+            options=["Home", "Facebook", "Twitter", "Instagram", "TikTok", "LinkedIn", "YouTube"],
+            icons=["house", "facebook", "twitter", "instagram", "tiktok", "linkedin", "youtube"],
+            menu_icon="cast",
+            default_index=0,
+        )
 
-Please use the sendbug(1) utility to report bugs in the system.
-Before reporting a bug, please try to reproduce it with the latest
-version of the code.  With bug reports, please try to ensure that
-enough information to reproduce the problem is enclosed, and if a
-known fix for it exists, include that as well.
+    st.title("Social Media Search Application")
+    st.write("Search for content on specific social media platforms using Google's Custom Search API.")
 
-fw$ ssh elk@192.168.100.147
-elk@192.168.100.147's password: 
-Linux osintelk 6.1.0-18-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.76-1 (2024-02-01) x86_64
+    if selected_option == "KizunaFinder":
+        st.subheader("🔍 KizunaFinder OSINT Tool")
+        kizunafinder_main()  # Run KizunaFinder tool
+        return  # Stop further execution to prevent conflicts
 
-The programs included with the Debian GNU/Linux system are free software;
-the exact distribution terms for each program are described in the
-individual files in /usr/share/doc/*/copyright.
+    # Search Input
+    query = st.text_input("Enter your search query:", "")
 
-Debian GNU/Linux comes with ABSOLUTELY NO WARRANTY, to the extent
-permitted by applicable law.
-Last login: Sun Mar 23 23:40:20 2025 from 192.168.100.1
-[oh-my-zsh] Would you like to update? [Y/n] n
-[oh-my-zsh] You can update manually by running `omz update`
-elk@osintelk:/home/elk $ 
-elk@osintelk:/home/elk $ ls     
-ahmia                crawl4ai     flowise                      Hawker           ML                 __pycache__  setup_snmpd.sh        testtools
-AnythingLLMDesktop   crawlee      Flowise                      journalsearch    nltk_data          quora.py     socmed                tools
-Arcgis               crawler      geo.py                       jupyter          node_modules       random1.py   SPR_Statistic         TorBot
-backup               data         getdocker.sh                 langchain-store  ollama             README.md    streamlit-multiapp    twidoc
-chatbot-tools        development  github-repo                  LICENSE          osint-tools        reddit       stream.py             twikit
-ChatGPT-API-Leakage  duckdb       go                           login.py         package.json       scout-lang   telegram_tracker.log  wireguard-install.sh
-conf.d               env          go1.21.6.linux-amd64.tar.gz  lowyat           package-lock.json  script       testserver.py
-elk@osintelk:/home/elk $ cd streamlit-multiapp                                     
-elk@osintelk:/home/elk/streamlit-multiapp $ ls
-streamlit-multiapp
-elk@osintelk:/home/elk/streamlit-multiapp $ cd streamlit-multiapp
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp git:(main) $ ls
-components  env  main.py  OSINT  README.md  requirements.txt  results
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp git:(main) $ cd OSINT             
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT git:(main) $ ls
-ghunt  gvision  Hawkerweb  holeheweb  kizunafinder  sherlock  socialpulse  tele  usernametoolweb  waybackWeb
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT git:(main) $ cd kizunafinder
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT/kizunafinder git:(main*) $ ls
-app.py  __pycache__  requirements.txt
-elk@osintelk:/home/elk/streamlit-multiapp/streamlit-multiapp/OSINT/kizunafinder git:(main*) $ nano app.py   
+    if query:
+        st.write(f"Showing results for: **{query}**")
 
-  GNU nano 7.2                                                                         app.py                                                                                   
+        platform_sites = {
+            "Facebook": "facebook.com",
+            "Twitter": "twitter.com",
+            "Instagram": "instagram.com",
+            "TikTok": "tiktok.com",
+            "LinkedIn": "linkedin.com",
+            "YouTube": "youtube.com",
+        }
+
+        # Select Site for Platform-Specific Queries
+        if selected_option == "Home":
+            st.subheader("Search All Platforms")
+            results = search_google_cse(query)
+        else:
+            st.subheader(f"{selected_option} Results")
+            results = search_google_cse(query, site=platform_sites.get(selected_option, None))
+
+        # Display Results
+        if results:
+            for result in results:
+                st.write(f"**{result['title']}**")
+                st.write(f"[{result['link']}]({result['link']})")
+                st.write(result["snippet"])
+                if result["image"] and is_valid_url(result["image"]):
+                    st.image(result["image"], width=400)
+                else:
+                    st.write("_No image available._")
+                st.write("---")
+        else:
+            st.write("No results found.")
+
+# Footer with Copyright
+    st.markdown("""
+    ---
+    © 2025, All rights reserved. 
+    Developed by ECLOGIC.
+    """)
 
 def search_google_cse(query, site=None):
     """
